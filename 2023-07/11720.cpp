515b45d525e0ab357b2170e627f627383f9df4ac
Joonheui <iuehnoojmik@naver.com>
2023-10-20 15:30:24 +0900

    Add files via upload

---
+++ 2023-07/11720.cpp
@@ -0,0 +1,26 @@
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//int main() {
+//	int test;
+//	
+//	cin >> test;
+//
+//	int sum = 0;
+//
+//	int i;
+//	string temp;
+//	cin >> temp;
+//
+//	int num; 
+//
+//	for (i = 0; i < test; i++) {	
+//		num = temp[i] - '0';
+//		sum += num;
+//	}
+//
+//	cout << sum << "\n";
+//
+//	return 0;
+//}
---
+++ 2023-07/2675.cpp
@@ -0,0 +1,30 @@
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//int main() {
+//	int num;
+//	cin >> num;
+//
+//	int i;
+//	int count;
+//	string str;
+//
+//	for (i = 0; i < num; i++) {
+//		cin >> count;
+//		cin >> str;
+//
+//		string temp = "";
+//		int j;
+//		for (j = 0; j < str.length(); j++) {
+//			int k;
+//			for (k = 0; k < count; k++) {
+//				temp.push_back(str[j]);
+//			}
+//		}
+//
+//		cout << temp << "\n";
+//	}
+//
+//	return 0;
+//}
